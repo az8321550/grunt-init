c3d5b03aa727437338e93106dd99af085c599a40
nobody <az8321550@gmail.com>
2015-10-24 22:14:37 +0800

    optimize :smile:

--- tasks/init.js
+++ tasks/init.js
@@ -1,33 +1,22 @@
 module.exports = function(grunt) {
-  var crypto = require('crypto');
+  var encrypt = require('cf-encrypt');
   var path = require('path');
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('init', 'The best Grunt plugin ever.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options();
-    if (!options.key) {
-      grunt.fail.warn('Missing key property.');
-    }
-    var key = options.key;
-
+    // Iterate over all specified file groups.
     this.files.forEach(function(f) {
+      // Concat specified files.
       var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           return false;
         } else {
-          var filename = path.basename(filepath);
           var newFilename = (grunt.file.isDir(options.dest) ? [filepath].join('.') : (options.ext) ? [options.dest, options.ext].join('.') : options.dest);
-          var contents = grunt.file.read(filepath);
-
-          if (!options.test2) {
-            var cipher = crypto.createCipher('aes-256-cbc', key)
-            cipher.update(contents, 'utf8', 'base64');
-            contents = cipher.final('base64')
-          }
-          else {
-            var decipher = crypto.createDecipher('aes-256-cbc', key);
-            decipher.update(contents, 'base64', 'utf8');
-            contents = decipher.final('utf8');
-          }
+          var contents = encrypt[(options.decrypt) ? 'decrypt' : 'encrypt'](options.key, grunt.file.read(filepath), 'hex');
           grunt.file.write(newFilename, contents);
           return true;
         }
