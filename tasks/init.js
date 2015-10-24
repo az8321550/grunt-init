fede20a6987c6a93c26391fbfa6a363275460629
nobody <az8321550@gmail.com>
2015-10-24 21:57:41 +0800

    optimize :smile:

--- tasks/init.js
+++ tasks/init.js
@@ -15,10 +15,10 @@ module.exports = function(grunt) {
           return false;
         } else {
           var filename = path.basename(filepath);
-          var newFilename = (grunt.file.isDir(options.dest) ? [filepath, 'encrypted'].join('.') : (options.ext) ? [options.dest, options.ext].join('.') : options.dest);
+          var newFilename = (grunt.file.isDir(options.dest) ? [filepath].join('.') : (options.ext) ? [options.dest, options.ext].join('.') : options.dest);
           var contents = grunt.file.read(filepath);
 
-          if (!options.decrypt) {
+          if (!options.test2) {
             var cipher = crypto.createCipher('aes-256-cbc', key)
             cipher.update(contents, 'utf8', 'base64');
             contents = cipher.final('base64')
@@ -27,9 +27,6 @@ module.exports = function(grunt) {
             var decipher = crypto.createDecipher('aes-256-cbc', key);
             decipher.update(contents, 'base64', 'utf8');
             contents = decipher.final('utf8');
-            var ext = filepath.split('.');
-            ext = ext[ext.length - 1];
-            newFilename = newFilename.split('.' + ext).join('');
           }
           grunt.file.write(newFilename, contents);
           return true;
